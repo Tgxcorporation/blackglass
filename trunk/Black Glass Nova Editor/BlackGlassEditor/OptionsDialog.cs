bf01be9f2640fbfc59805059b5484d88e1255917
Tgxcorporation <Tgxcorporation@536c1c60-8950-11de-9095-6b49e043a1b4>
2011-03-10 11:37:28 +0000

    BGN Editor: Added support for plugin parameters

---
+++ trunk/Black Glass Nova Editor/BlackGlassEditor/OptionsDialog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BlackGlassEditor
+{
+    public partial class OptionsDialog : Form
+    {
+        public OptionsDialog()
+        {
+            InitializeComponent();
+        }
+
+        private string mId;
+        public string Id
+        {
+            get { return mId; }
+            set { textBoxId.Text = value; }
+        }
+
+        private string mDescription;
+        public string Description
+        {
+            get { return mDescription; }
+            set { textBoxDescription.Text = value; }
+        }
+
+        private string mParameter;
+        public string Parameter
+        {
+            get { return mParameter; }
+            set { textBoxParameter.Text = value; }
+        }
+
+        private string mHover;
+        public string Hover
+        {
+            get { return mHover; }
+            set { textBoxHover.Text = value; }
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            mId = textBoxId.Text;
+            mDescription = textBoxDescription.Text;
+            mParameter = textBoxParameter.Text;
+            mHover = textBoxHover.Text;
+        }
+
+    }
+}
